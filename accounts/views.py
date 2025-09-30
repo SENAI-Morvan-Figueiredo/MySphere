6e62e7ea0cca25a8dd2109badb73012a990439c3
Pedro Odake <pedroodake@gmail.com>
2025-09-30 08:38:32 -0300

    função de login e logout, e funcionamento de quem está logado finalizada

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
 from .models import User
 from .forms import UserForm
 from django.urls import reverse_lazy
-# Create your views here.
+from django.views import View
 
+# Create your views here.
 
 class Users(ListView):
     model = User
     template_name = "accounts/account_list.html"
+    context_object_name = "users"
+
+class Home(ListView):
+    model = User
+    template_name = "accounts/account_home.html"
     context_object_name = "users"
 
 class novo(CreateView):
@@ -16,3 +24,7 @@ class novo(CreateView):
     form_class = UserForm
     template_name = "accounts/add.html"
     success_url = reverse_lazy('users')
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'  
+    redirect_authenticated_user = True     
