10d226d7c578179d73936e68c9597c489203bdf5
Pedro Odake <pedroodake@gmail.com>
2025-10-16 17:00:05 -0300

    tela de chat com extends, podendo ver a conversa na mesma tela faltando ajustes no css e conversas existentes

--- MySphere/settings.py
+++ MySphere/settings.py
@@ -35,9 +35,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django_cryptography",
-
-
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,7 +44,6 @@ INSTALLED_APPS = [
     'accounts',
     'chat',
     'tenants',
-    'fead'
 ]
 
 AUTH_USER_MODEL = "accounts.User"
--- fead/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeadConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fead'
