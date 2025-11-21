4daca3456263e7b2eb78037458c285dd64dffeec
Guilherme Pereira <gprsilva2008@gmail.com>
2025-11-21 13:15:21 -0300

    Adicionei long polling no chat

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('visualizar/<int:msg_id>/<str:tipo>/', views.visualizar_arquivo, name='visualizar_arquivo'),
     path('criar_chat_ajax/', views.criar_chat_ajax, name='criar_chat_ajax'),
     path('atualizar_chats/', views.atualizar_chats, name='atualizar_chats'),
+    path("chat/<int:chat_id>/atualizar/", views.atualizar_mensagens, name="atualizar_mensagens"),
+
 ]
