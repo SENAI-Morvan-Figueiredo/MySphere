c7e6b0cd104a2d12740b866e61ea00433ce21176
Pedro Odake <pedroodake@gmail.com>
2025-12-01 16:25:11 -0300

    ajuste no settings.py

--- MySphere/settings.py
+++ MySphere/settings.py
@@ -72,7 +72,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-DEFAULT_FROM_EMAIL = f"Sua Plataforma {os.environ.get("EMAIL_USER")}"
 
 
 AUTHENTICATION_BACKENDS = [
